885f36ccedf57a2c73d255c7f9de3911c040726b
Sam Hamm <sam@codefellows.com>
2018-02-19 10:45:23 -0800

    Middle of class 2/19

---
+++ class-11-av-practical-clicktracker/demo-audio-video/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var levees = document.getElementById('levees');
+var randomizer = document.getElementById('randomizer');
+randomizer.addEventListener('click', function(){
+  levees.volume = Math.random();
+});
